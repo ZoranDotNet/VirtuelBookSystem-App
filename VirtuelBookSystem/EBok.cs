7030dd2567036dd4603edaef5a131c2f04f875f7
Ester Zetterlund <ester.zetterlund.NET23@edu.edugrade.se>
2023-11-08 14:55:11 +0100

    EBok som ärver

---
+++ VirtuelBookSystem/EBok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtuelBookSystem
+{
+    internal class Ebok : Bok
+    {
+        public string filStorlek { get; set; }
+        public string downloadUrl { get; set; }
+
+        public override void SkrivutInfo()
+        {
+            base.SkrivUtInfo();
+            Console.WriteLine($"Filstorlek: {filStorlek} \nDownloadUrl: {downloadUrl}");
+        }
+    }
+}
